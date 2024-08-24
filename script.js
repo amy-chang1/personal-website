e274761d8174c94b7d5fe5ccb15a766b9b92cc52
amy-chang1 <amychang3201@gmail.com>
2024-08-24 18:53:02 -0400

    new ui, new navbar, new buttons & links

---
+++ script.js
@@ -0,0 +1,11 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function proceed() {
+    window.location.href = "https://amyspace.netlify.app/";
+}
